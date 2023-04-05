6661b23fd5facef6ac4058fc7e60e9d68d42fb33
Kunal0220 <ksrivastava0220@gmail.com>
2023-04-05 15:21:31 +0530

    Backend Updated

--- index.js
+++ index.js
@@ -4,37 +4,42 @@ const axios = require('axios');
 
 const app = express();
 require('dotenv').config();
+const PORT = process.env.PORT
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-const PORT = process.env.PORT
-
-
-const getData = async()=>{
-    var location = 'Bengaluru'
-    var link = `https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${location}&aqi=yes`
-
-    let res = await axios.get(link);
-
-    let data = res.data;
-    // console.log(data);
-
-}
-
-getData();
+// app.set('view engine', 'ejs');
 
 
 
-app.get('/' ,(req,res)=>{
-    res.json('Helllo World');
-} )
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+
 
 app.post('/',(req,res)=>{
-    const fname = req.body.name;
-    res.send(fname)
+    const location = req.body.name;
+    res.send(location)
+    res.send(getData(location))
+    console.log(getData(location))
+    res.end()
 })
 
+const getData = async(location)=>{
+    // var location = 'Bengaluru'
+    var link = `https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${location}&aqi=yes`
+    
+    let res = await axios.get(link);
+    
+    let data = res.data;
+    
+    return(data);
+    
+}
 
 app.listen(PORT , ()=>{
     console.log(`App is running on PORT ${PORT}`)
